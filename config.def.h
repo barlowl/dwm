eaa28e74617357ac64dc675a56291af68dc8f311
luke <barlowlukew@gmail.com>
2023-12-27 20:56:33 -0500

    fix muting audio

--- config.def.h
+++ config.def.h
@@ -69,7 +69,7 @@ static const char *termcmd[]  = { "st", NULL };
 /* audio control */
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",    NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",    NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "toggle", NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
